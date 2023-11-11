3e1c0a93e8ab47b9716f17c76d0240d771fbe63a
burnjoe <sabanajholo@gmail.com>
2023-11-11 12:44:20 +0800

    added SPA experience to page load

--- resources/views/livewire/inc/subnav_game.blade.php
+++ resources/views/livewire/inc/subnav_game.blade.php
@@ -19,21 +19,21 @@
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link ps-1"
-                                href="{{ route('games.matches', ['event' => $event->id, 'game' => $game->id]) }}">Matches</a>
+                                wire:navigate href="{{ route('games.matches', ['event' => $event->id, 'game' => $game->id]) }}">Matches</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link"
-                                href="{{ route('games.athletes', ['event' => $event->id, 'game' => $game->id]) }}">Athletes</a>
+                                wire:navigate href="{{ route('games.athletes', ['event' => $event->id, 'game' => $game->id]) }}">Athletes</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link"
-                                href="{{ route('games.settings', ['event' => $event->id, 'game' => $game->id]) }}">Settings</a>
+                                wire:navigate href="{{ route('games.settings', ['event' => $event->id, 'game' => $game->id]) }}">Settings</a>
                         </li>
                     </ul>
                 </div>
                 <div class="d-flex flex-row align-items-center">
                     <div class="pe-1 py-2">
-                        <a href="{{ route('events.show', ['event' => $event->id]) }}" class="custBtn custBtn-light"><i
+                        <a wire:navigate href="{{ route('events.show', ['event' => $event->id]) }}" class="custBtn custBtn-light"><i
                                 class="bi bi-arrow-left"></i>&nbsp Back to All Games</a>
                     </div>
                 </div>
