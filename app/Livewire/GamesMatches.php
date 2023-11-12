8f4cadcf2254af9565b9b43b7cc5c06f4949d83f
burnjoe <sabanajholo@gmail.com>
2023-11-12 14:00:07 +0800

    added generate matches + announce/denounce winner

--- app/Livewire/GamesMatches.php
+++ app/Livewire/GamesMatches.php
@@ -16,6 +16,9 @@ class GamesMatches extends Component
     public $event;
     public $game;
 
+    public $round = '1';
+    public $winner_id;
+
     public $search = "";
 
 
@@ -36,65 +39,196 @@ class GamesMatches extends Component
         return view('livewire.games.matches', [
             'matches' => GameMatch::with('athlete1', 'athlete2', 'winner', 'athlete1.team', 'athlete2.team', 'winner.team')
                 ->where('game_id', $this->game->id)
+                ->when($this->search, function ($query) {
+                    $query->whereHas('athlete1', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('athlete2', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('winner', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('athlete1.team', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('athlete2.team', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('winner.team', fn ($query) => $query->search($this->search));
+                })
                 ->where(function ($query) {
-                    $query->whereHas('athlete1', function ($subquery) {
-                        $subquery->search($this->search);
-                    })
-                        ->orWhereHas('athlete2', function ($subquery) {
-                            $subquery->search($this->search);
-                        })
-                        ->orWhereHas('winner', function ($subquery) {
-                            $subquery->search($this->search);
-                        })
-                        ->orWhereHas('athlete1.team', function ($subquery) {
-                            $subquery->search($this->search);
-                        })
-                        ->orWhereHas('athlete2.team', function ($subquery) {
-                            $subquery->search($this->search);
-                        })
-                        ->orWhereHas('winner.team', function ($subquery) {
-                            $subquery->search($this->search);
-                        });
+                    $query->round($this->round);
                 })
                 ->paginate(15)
         ]);
     }
 
     /**
+     * Validation rules
+     */
+    public function rules()
+    {
+        return [
+            'winner_id' => 'required',
+        ];
+    }
+
+    /**
+     * Validation messages
+     */
+    public function messages()
+    {
+        return [];
+    }
+
+    /**
+     * Validation attributes
+     */
+    public function validationAttributes()
+    {
+        return [
+            'winner_id' => 'round winner',
+        ];
+    }
+
+    /**
+     * Update some attributes when sex field is updated
+     */
+    public function updatedWinnerId()
+    {
+        try {
+            $this->announceWinner($this->winner_id);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
      * Generate matches
      */
-    public function generateMatches() {
-        $athletes = Athlete::all()->shuffle(); // Shuffle the athletes randomly
+    public function generateMatches()
+    {
+        /**
+         * Matchmaking Rules:
+         * 
+         * If Matchmake for round 1:
+         * 1. Athlete must be a different athlete
+         * 2. Athlete must be on a different team
+         * 3. Athlete must be on the same event
+         * 4. Athlete must be on the same game (that includes its sex and weight category) + (age range: to follow)
+         * 5. Athlete must have no existing match on this event
+         * 
+         * If Matchmake for round >= 2:
+         * 1. Athlete must be a different athlete
+         * 2. Athlete must be on a different team
+         * 3. Athlete must be on the same event
+         * 4. Athlete must be on the same game (that includes its sex and weight category) + (age range: to follow)
+         * 5. Athlete must be a winner athlete of the previous round (current round - 1)
+         * 
+         * Before to proceed to next round:
+         * 1. Check if all current round matches has winner
+         * 2. Check if round number is valid and not skipping round (e.g. Round 1 -> Round 4) 
+         */
+
+
+
+
+
+
+        // Shuffle the athletes randomly
+        // $athletes = Athlete::all()->shuffle();
+
+        // $athletes = Athlete::where('game_id', $this->game->id)->get()->shuffle();
+
+        $athletes = Athlete::noMatchesInSameGame($this->game->id)->get()->shuffle();
+        // dd($athletes);
 
         // Create match pairings
         $matches = [];
 
         while ($athletes->count() >= 2) {
-            $athlete1 = $athletes->shift();
+            $athlete = $athletes->shift();
 
             // Randomly select an opponent from the remaining athletes
-            $opponent = $athletes->random();
+            // $opponent = $athletes->random();
+            $opponent = $athletes->shift();
 
             // Remove the opponent from the remaining athletes
-            $athletes = $athletes->reject(function ($athlete) use ($opponent) {
-                return $athlete->id === $opponent->id;
-            });
+            // $athletes = $athletes->reject(function ($athlete) use ($opponent) {
+            //     return $athlete->id === $opponent->id;
+            // });
 
-            // Create a match between $athlete1 and $opponent
+            // Create a match between $athlete and $opponent
             $match = GameMatch::create([
-                'round' => 1, // You can adjust the round as needed
-                'athlete1_id' => $athlete1->id,
+                'round' => $this->round,
+                'game_id' => $this->game->id,
+                'athlete1_id' => $athlete->id,
                 'athlete2_id' => $opponent->id,
-                'winner_id' => null, // Winner not set initially
             ]);
 
             $matches[] = $match;
         }
 
+        // dd($matches);
+
         // Handle any remaining athlete (if the number of athletes is odd)
         if ($athletes->count() === 1) {
-            $unmatchedAthlete = $athletes->first();
+            GameMatch::create([
+                'round' => $this->round,
+                'game_id' => $this->game->id,
+                'athlete1_id' => $athletes->first()->id,
+            ]);
+            // $unmatchedAthlete = $athletes->first();
             // You can either assign them a bye or handle it as needed for your specific tournament rules.
         }
+    }
+
+    /**
+     * Announce round winner
+     */
+    public function announceWinner($winner_id)
+    {
+        try {
+            $this->authorize('manage events');
+        } catch (\Throwable $th) {
+            session()->flash('danger', 'Unauthorized action.');
+            return $this->redirectRoute('games.matches', ['event' => $this->event->id, 'game' => $this->game->id], navigate: true);
+        }
+
+        $validated = $this->validate();
+
+        $winner = GameMatch::where('round', $this->round)
+            ->where('game_id', $this->game->id)
+            ->where('athlete1_id', $winner_id)
+            ->orWhere('athlete2_id', $this->winner_id)
+            ->first();
+
+        $winner->update($validated);
+
+        $this->reset('winner_id');
+    }
+
+    /**
+     * Denounce round winner, this undo the announcement of winner
+     */
+    public function denounceWinner($winner_id)
+    {
+        try {
+            $this->authorize('manage events');
+        } catch (\Throwable $th) {
+            session()->flash('danger', 'Unauthorized action.');
+            return $this->redirectRoute('games.matches', ['event' => $this->event->id, 'game' => $this->game->id], navigate: true);
+        }
+
+        $winner = GameMatch::where('round', $this->round)
+            ->where('game_id', $this->game->id)
+            ->where('winner_id', $winner_id)
+            ->first();
+
+        if ($winner) {
+            $winner->update([
+                'winner_id' => null,
+            ]);
+
+            $this->reset('winner_id');
+        }
+    }
+
+    /**
+     * Proceed to next round, creates new round for new matches
+     */
+    public function proceedNextRound()
+    {
     }
 }
--- resources/views/livewire/games/matches.blade.php
+++ resources/views/livewire/games/matches.blade.php
@@ -2,6 +2,9 @@
     @include('livewire.inc.subnav_game')
 
     <div class="container text-dark pb-3 px-1">
+        {{-- Alerts --}}
+        @include('livewire.inc.alerts')
+
         <div class="container-fluid d-flex justify-content-between mb-3 pt-3">
             <div class="px-3">
                 <h5 class="fw-bold">Matches</h5>
@@ -10,25 +13,90 @@
             <div class="d-flex justify-content-end col">
                 {{-- Search --}}
                 @include('livewire.inc.search')
-                <a wire:navigate href="#" class="custBtn custBtn-light me-3"><i class="bi bi-diagram-2"></i>&nbsp Generate Matches</a>
+                <button wire:click="generateMatches" class="custBtn custBtn-light me-3">Proceed to Next Round &nbsp<i
+                        class="bi bi-arrow-right"></i></button>
             </div>
         </div>
 
         <div class="mx-4 mb-3 bg-white" style="overflow-x: auto;  box-shadow: 0px 5px 8px 0 rgba(0, 0, 0, 0.2);">
             <table class="table table-striped table-hover mb-0">
-                <thead class="table-dark text-light">
+                <thead class="table-dark text-light" style="white-space: nowrap;">
                     <th scope="col">Red Corner</th>
                     <th scope="col">Blue Corner</th>
-                    <th scope="col">Round</th>
+                    <th scope="col">
+                        <ul class="navbar-nav ms-auto me-2">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle py-0" role="button" data-bs-toggle="dropdown"
+                                    data-bs-auto-close="outside" aria-expanded="false">
+                                    Round
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                    <li>
+                                        <a class="dropdown-item">
+                                            <input wire:model.live.debounce.300ms="round" class="form-check-input me-1"
+                                                type="radio" value="1" id="1">
+                                            <label class="form-check-label fs-6 fw-normal" for="1">Round 1</label>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </th>
                     <th scope="col">Round Winner</th>
+                    <th scope="col">Action</th>
                 </thead>
-                <tbody>
+                <tbody style="white-space: nowrap;">
                     @foreach ($matches as $match)
                     <tr scope="row" wire:key="{{ $match->id }}">
-                        <td>{{ $match->athlete1->last_name }}</td>
-                        <td>{{ $match->athlete2->last_name }}</td>
+                        <td>
+                            <span class="badge text-bg-danger py-1 me-1">{{ $match->athlete1->team->name ?? '' }}</span>
+                            {{ $match->athlete1->last_name ?? '' }}
+                        </td>
+                        <td>
+                            <span class="badge text-bg-primary py-1 me-1">{{ $match->athlete2->team->name ?? ''
+                                }}</span>
+                            {{ $match->athlete2->last_name ?? '' }}
+                        </td>
                         <td>{{ $match->round }}</td>
-                        <td>{{ $match->winner->last_name }}</td>
+                        <td>
+                            @if($match->winner)
+
+                            @php
+                            $badgeColor = $match->winner->team->id === $match->athlete1->team->id ? 'danger' :
+                            'primary';
+                            @endphp
+
+                            <span class="badge text-bg-{{$badgeColor}} py-1 me-1">{{ $match->winner->team->name
+                                }}</span>
+                            {{ $match->winner->last_name }}
+                            @else
+                            <div>
+                                <select wire:model.live.debounce.300ms="winner_id" name="winner"
+                                    class="form-select custFormSelect @error('sex') is-invalid @enderror"
+                                    aria-label=".form-select example" style="font-size: 12;" required>
+                                    <option class="custOption" hidden>Select Round Winner</option>
+                                    <option class="custOption" value="{{ $match->athlete1->id }}">{{
+                                        $match->athlete1->team->name }}</option>
+                                    @if($match->athlete2)
+                                    <option class="custOption" value="{{ $match->athlete2->id }}">{{
+                                        $match->athlete2->team->name }}</option>
+                                    @endif
+                                </select>
+                                @error('winner')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            @endif
+                        </td>
+                        <td>
+                            @if($match->winner)
+                            <button wire:click="denounceWinner({{$match->winner->id}})" class="custBtn custBtn-light"><i
+                                    class="bi bi-arrow-counterclockwise"></i>&nbsp
+                                Undo</button>
+                            @endif
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
@@ -38,9 +106,20 @@
             @if($matches->total() == 0)
             <div class="d-flex justify-content-center align-items-center my-5">
                 @if(empty($search))
-                <h4>No existing records.</h4>
+                <div class="d-flex row">
+                    <div class="d-flex justify-content-center">
+                        <h4>No existing records.</h4>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button wire:click="generateMatches" class="custBtn custBtn-light me-3"><i
+                                class="bi bi-diagram-2"></i>&nbsp
+                            Generate Matches</button>
+                    </div>
+                </div>
                 @else
-                <h4>No records found for matching "{{$search}}".</h4>
+                <div class="d-flex justify-content-center">
+                    <h4>No records found for matching "{{$search}}".</h4>
+                </div>
                 @endif
             </div>
             @endif
