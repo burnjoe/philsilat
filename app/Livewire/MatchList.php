b65e46a9b8ae62ae70687593cfe57f8862515b58
burnjoe <sabanajholo@gmail.com>
2023-11-25 17:55:59 +0800

    componentized match tables + fixed pagination bug

---
+++ app/Livewire/MatchList.php
@@ -0,0 +1,272 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Game;
+use App\Models\Event;
+use App\Models\Athlete;
+use Livewire\Component;
+use App\Models\GameMatch;
+use Livewire\WithPagination;
+
+class MatchList extends Component
+{
+    use WithPagination;
+
+    public $event;
+    public $game;
+
+    public $round;
+    public $winner_id;
+
+    public $search = "";
+
+
+    /**
+     * Renders the view
+     */
+    public function render()
+    {
+        return view('livewire.components.match-list', [
+            'matches' => GameMatch::with('athlete1', 'athlete2', 'winner', 'athlete1.team', 'athlete2.team', 'winner.team')
+                ->where('game_id', $this->game->id)
+                ->when($this->search, function ($query) {
+                    $query->whereHas('athlete1', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('athlete2', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('winner', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('athlete1.team', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('athlete2.team', fn ($query) => $query->search($this->search))
+                        ->orWhereHas('winner.team', fn ($query) => $query->search($this->search));
+                })
+                ->round($this->round)
+                ->paginate(3),
+            'round' => $this->round,
+            'rounds' => GameMatch::select('round')
+                ->where('game_id', $this->game->id)
+                ->groupBy('round')
+                ->get(),
+            'is_round_completed' => !GameMatch::select('winner_id')
+                ->where('game_id', $this->game->id)
+                ->where('round', $this->round)
+                ->where('winner_id', null)
+                ->count() > 0,
+        ]);
+    }
+
+    /**
+     * Validation rules
+     */
+    public function rules()
+    {
+        return [
+            'winner_id' => 'required|integer',
+        ];
+    }
+
+    /**
+     * Validation messages
+     */
+    public function messages()
+    {
+        return [];
+    }
+
+    /**
+     * Validation attributes
+     */
+    public function validationAttributes()
+    {
+        return [
+            'winner_id' => 'round winner',
+        ];
+    }
+
+    /**
+     * Update some attributes when sex field is updated
+     */
+    public function updatedWinnerId()
+    {
+        try {
+            $this->announceWinner($this->winner_id);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
+     * Generate match pairings
+     *
+     * Matchmaking Rules:
+     * 
+     * If Matchmake for round 1:
+     * 1. Athlete must be a different athlete
+     * 2. Athlete must be on a different team
+     * 3. Athlete must be on the same event
+     * 4. Athlete must be on the same game (that includes its sex and weight category) + (age range: to follow)
+     * 5. Athlete must have no existing match on this event
+     * 
+     * If Matchmake for round >= 2:
+     * 1. Athlete must be a different athlete
+     * 2. Athlete must be on a different team
+     * 3. Athlete must be on the same event
+     * 4. Athlete must be on the same game (that includes its sex and weight category) + (age range: to follow)
+     * 5. Athlete must be a winner athlete of the previous round (current round - 1)
+     * 
+     * Before to proceed to next round:
+     * 1. Check if all current round matches has winner
+     * 2. Check if round number is valid and not skipping round (e.g. Round 1 -> Round 4) 
+     *
+     */
+    public function generateMatches()
+    {
+        // Only generate match pairings for ongoing events
+        if ($this->event->status !== "ONGOING") {
+            session()->flash('danger', 'Something unexpected happened. Please refresh the page and try again.');
+
+            return $this->redirectRoute('games.matches', ['event' => $this->event->id, 'game' => $this->game->id], navigate: true);
+        }
+
+        // Get latest round number
+        $this->round = GameMatch::select('round')
+            ->where('game_id', $this->game->id)
+            ->groupBy('round')
+            ->latest('round')
+            ->first();
+
+        // Shuffle the athletes randomly
+        if (!$this->round) {
+            $athletes = Athlete::noMatchesInSameGame($this->game->id)->get()->shuffle();
+        } else {
+            $this->round = $this->round->round;
+            $athletes = Athlete::winnersOfSameRound($this->game->id, $this->round)->get()->shuffle();
+            $this->closeMatchesOfRound($this->round);
+        }
+
+        // Use to check if all match pairings of this round is closed
+        $checkMatch = GameMatch::where('game_id', $this->game->id)
+            ->where('round', $this->round)
+            ->value('is_closed') ?? true;
+
+        // Checks if there's an athlete and ensure all pairs have winners
+        if ($athletes->isEmpty() || !$checkMatch) {
+            session()->flash('danger', 'Something unexpected happened. Please refresh the page and try again.');
+
+            return $this->redirectRoute('games.matches', ['event' => $this->event->id, 'game' => $this->game->id], navigate: true);
+        }
+
+        // Create match pairings for new round
+        $this->round += 1;
+
+        // Gets last game number
+        $game_no = GameMatch::where('game_id', $this->game->id)
+            ->count();
+
+        // Create match pairings
+        while ($athletes->count() >= 2) {
+            $game_no++;
+            $athlete = $athletes->shift();
+            $opponent = $athletes->shift();
+
+            // Create a match between $athlete and $opponent
+            GameMatch::create([
+                'round' => $this->round,
+                'game_no' => $game_no,
+                'game_id' => $this->game->id,
+                'athlete1_id' => $athlete->id,
+                'athlete2_id' => $opponent->id,
+            ]);
+        }
+
+        // Handle any remaining athlete
+        if ($athletes->isNotEmpty()) {
+            $game_no++;
+
+            GameMatch::create([
+                'round' => $this->round,
+                'game_no' => $game_no,
+                'game_id' => $this->game->id,
+                'athlete1_id' => $athletes->first()->id,
+            ]);
+        }
+
+        session()->flash('success', 'The pairings for the round have been successfully generated.');
+
+        return $this->redirectRoute('games.matches', ['event' => $this->event->id, 'game' => $this->game->id], navigate: true);
+    }
+
+    /**
+     * Announce round winner
+     */
+    public function announceWinner($winner_id)
+    {
+        try {
+            $this->authorize('manage events');
+        } catch (\Throwable $th) {
+            session()->flash('danger', 'Unauthorized action.');
+            return $this->redirectRoute('games.matches', ['event' => $this->event->id, 'game' => $this->game->id], navigate: true);
+        }
+
+        $validated = $this->validate();
+
+        $winner = GameMatch::where('game_id', $this->game->id)
+            ->where('round', $this->round)
+            ->where(function ($query) use ($winner_id) {
+                $query->where('athlete1_id', $winner_id)
+                    ->orWhere('athlete2_id', $winner_id);
+            })
+            ->first();
+
+        if ($winner) {
+            $winner->update($validated);
+
+            $this->reset('winner_id');
+        }
+    }
+
+    /**
+     * Denounce round winner, this undo the announcement of winner
+     */
+    public function denounceWinner($winner_id)
+    {
+        try {
+            $this->authorize('manage events');
+        } catch (\Throwable $th) {
+            session()->flash('danger', 'Unauthorized action.');
+            return $this->redirectRoute('games.matches', ['event' => $this->event->id, 'game' => $this->game->id], navigate: true);
+        }
+
+        $winner = GameMatch::where('round', $this->round)
+            ->where('game_id', $this->game->id)
+            ->where('winner_id', $winner_id)
+            ->first();
+
+        if ($winner) {
+            $winner->update([
+                'winner_id' => null,
+            ]);
+
+            $this->reset('winner_id');
+        }
+    }
+
+    /**
+     * Close matches, means that action made to this match will be irreversible
+     */
+    public function closeMatchesOfRound($round)
+    {
+        $matches = GameMatch::where('game_id', $this->game->id)
+            ->where('round', $round);
+
+        // Checks if all match pairs of the current round has winner
+        if (!$matches->get()->where('winner_id', null)->count() > 0) {
+            if ($matches->count() === 1) {
+                $this->game->update(['is_completed' => true]);
+
+                session()->flash('success', 'The game has been successfully completed.');
+            }
+
+            $matches->where('is_closed', false)
+                ->update(['is_closed' => true]);
+        }
+    }
+}
---
+++ resources/views/livewire/components/match-list.blade.php
@@ -0,0 +1,139 @@
+<div>
+    <div class="container-fluid d-flex justify-content-between mb-3 pt-3">
+        <div class="px-3">
+            <h5 class="fw-bold">
+                @if($matches->count() === 1)
+                Finals
+                @elseif($matches->count() === 2)
+                Semi-Finals
+                @else
+                Elimination
+                @endif
+
+                (Round {{ $round }})
+
+                @if($matches->count() > 0)
+                @if($matches->first()->is_closed)
+                <span class="badge text-bg-success py-1 ms-1">COMPLETED</span>
+                @endif
+                @endif
+            </h5>
+        </div>
+        <div class="d-flex justify-content-end col">
+            {{-- Search --}}
+            @include('livewire.inc.search')
+            {{-- @dd($matches) --}}
+            @if($rounds->count() >= 1 && $matches->total() > 1 && $is_round_completed && !$matches->first()->is_closed)
+            <button wire:click="generateMatches" class="custBtn custBtn-light me-3">Generate Next Round &nbsp<i
+                    class="bi bi-arrow-right"></i></button>
+            @elseif($matches->total() == 1 && !$matches->first()->is_closed && $matches->first()->winner_id)
+            <button wire:click="closeMatchesOfRound({{$round}})" class="custBtn custBtn-light me-3"><i
+                    class="bi bi-lock-fill"></i>&nbsp Mark Game Complete</button>
+            @endif
+        </div>
+    </div>
+
+    <div class="mx-4 mb-3 bg-white" style="overflow-x: auto;  box-shadow: 0px 5px 8px 0 rgba(0, 0, 0, 0.2);">
+        <table class="table table-striped table-hover mb-0">
+            <thead class="table-dark text-light" style="white-space: nowrap;">
+                <th scope="col">Game #</th>
+                <th scope="col">Red Corner</th>
+                <th scope="col">Blue Corner</th>
+                {{-- <th scope="col">Round</th> --}}
+                <th scope="col">Round Winner</th>
+                @if($matches->where('round', $round)->where('is_closed', true)->count() != $matches->where('round',
+                $round)->count())
+                <th scope="col">Action</th>
+                @endif
+            </thead>
+            <tbody style="white-space: nowrap;">
+                @foreach ($matches as $match)
+                <tr scope="row" wire:key="{{ $match->id }}">
+                    <td>
+                        {{ $match->game_no }}
+                    </td>
+                    <td>
+                        <span class="badge text-bg-danger py-1 me-1">{{ $match->athlete1->team->name ?? '' }}</span>
+                        {{ $match->athlete1->last_name ?? 'N/A' }}
+                    </td>
+                    <td>
+                        <span class="badge text-bg-primary py-1 me-1">{{ $match->athlete2->team->name ?? ''
+                            }}</span>
+                        {{ $match->athlete2->last_name ?? 'N/A' }}
+                    </td>
+                    {{-- <td>{{ $match->round }}</td> --}}
+                    <td>
+                        @if($match->winner)
+
+                        @php
+                        $badgeColor = $match->winner->team->id === $match->athlete1->team->id ? 'danger' :
+                        'primary';
+                        @endphp
+
+                        <span class="badge text-bg-{{$badgeColor}} py-1 me-1">{{ $match->winner->team->name
+                            }}</span>
+                        {{ $match->winner->last_name }}
+                        @else
+                        <div>
+                            <select wire:model.live.debounce.300ms="winner_id" name="winner"
+                                class="form-select custFormSelect @error('sex') is-invalid @enderror"
+                                aria-label=".form-select example" style="font-size: 12;" required>
+                                <option class="custOption" hidden>Select Round Winner</option>
+                                <option class="custOption" value="{{ $match->athlete1->id }}">{{
+                                    $match->athlete1->team->name }}</option>
+                                @if($match->athlete2)
+                                <option class="custOption" value="{{ $match->athlete2->id }}">{{
+                                    $match->athlete2->team->name }}</option>
+                                @endif
+                            </select>
+                            @error('winner')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                        @endif
+                    </td>
+                    @if($matches->where('round', $round)->where('is_closed', true)->count() != $matches->where('round',
+                    $round)->count())
+                    <td>
+                        @if($match->winner && !$match->is_closed)
+                        <button wire:click="denounceWinner({{$match->winner->id}})" class="custBtn custBtn-light"><i
+                                class="bi bi-arrow-counterclockwise"></i>&nbsp
+                            Undo</button>
+                        @endif
+                    </td>
+                    @endif
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        {{-- No Records Found --}}
+        @if($matches->total() == 0)
+        <div class="d-flex justify-content-center align-items-center my-5">
+            @if(empty($search))
+            <div class="d-flex row">
+                <div class="d-flex justify-content-center">
+                    <h4>No existing records.</h4>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button wire:click="generateMatches" class="custBtn custBtn-light me-3"><i
+                            class="bi bi-diagram-2"></i>&nbsp
+                        Generate Matches</button>
+                </div>
+            </div>
+            @else
+            <div class="d-flex justify-content-center">
+                <h4>No records found for matching "{{$search}}".</h4>
+            </div>
+            @endif
+        </div>
+        @endif
+    </div>
+
+    {{-- Pagination Links --}}
+    <div class="mx-3 mt-4">
+        {{ $matches->links() }}
+    </div>
+</div>
