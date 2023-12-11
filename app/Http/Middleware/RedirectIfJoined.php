54489f491eaffd941ed4e67562843983b927d7fb
burnjoe <sabanajholo@gmail.com>
2023-12-11 20:09:08 +0800

    removed unused imports

--- app/Http/Middleware/RedirectIfJoined.php
+++ app/Http/Middleware/RedirectIfJoined.php
@@ -5,12 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use App\Models\Admin;
 use App\Models\Coach;
-use App\Models\Event;
-use Illuminate\Console\View\Components\Component;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Livewire\Component as LivewireComponent;
-use Livewire\Livewire;
 use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfJoined
